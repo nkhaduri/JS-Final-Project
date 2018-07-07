614c7da755d71f1a07d2e29aa05a2d5166dbed66
nino <nkhad15@freeuni.edu.ge>
2018-07-07 12:05:08 +0400

    latest update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+require("babel-core/register");
+require("babel-polyfill");
 
 gulp.task('default', function(){
   return gulp.src('docs/**/*.js')
